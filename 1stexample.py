04a66c8c585ce4a55147932e5dc20d2e3b7ed6cc
Atiyeh996 <atahavorgar@mun.ca>
2021-06-13 21:26:38 +0000

    add fuction and plt style

--- 1stexample.py
+++ 1stexample.py
@@ -5,40 +5,39 @@ import random
 from numpy import *
 
 
+plt.style.use('Solarize_Light2')
+def msd(N):
+    mean=linspace(0,0,N,dtype=float)
+    count=0
 
-mean=linspace(0,0,1000,dtype=float)
-count=0
-
-dock=range(1,33)
-for i in dock:
-    s=[-1,1]
-    steps=random.choice(s, size=(1000,2))
-    data = cumsum(steps, axis=0)
-    mean += tidynamics.msd(data)
-    count+=1
-
-
-mean /= count
-mean = mean[1:1000//2]
-
-l = arange(1,500)
-
-plt.plot(l, mean, label='Random walk (num.)')
-
-plt.plot(l, 2*l, label='Random walk (theo.)')
-
-plt.plot(l[1:], tidynamics.msd(l)[1:],label='Constant velocity (num.)', ls='--')
-plt.plot(l[1:], l[1:]**2,label='Constant velocity (theo.)', ls='--')
+    dock=range(1,50)
+    for i in dock:
+        s=[-1,1]
+        steps=random.choice(s, size=(N,2))
+        data = cumsum(steps, axis=0)
+        mean += tidynamics.msd(data)
+        count+=1
 
 
-plt.loglog()
-plt.legend()
-plt.xlabel('time')
-plt.ylabel('mean square displacement')
-plt.title('Examples for the mean-square displacement')
-plt.savefig('2.png', dpi=100, bbox_inches='tight')
+    mean /= count
+    mean = mean[1:N//2]
+
+    l = arange(1,N//2)
+
+    plt.plot(l, mean, label='Random walk (num.)')
+
+    plt.plot(l, 2*l, label='Random walk (theo.)')
+
+    plt.plot(l[1:], tidynamics.msd(l)[1:],label='Constant velocity (num.)', ls='--')
+    plt.plot(l[1:], l[1:]**2,label='Constant velocity (theo.)', ls='--')
 
 
-plt.show()
-
-
+    plt.loglog()
+    plt.legend()
+    plt.xlabel('time')
+    plt.ylabel('mean square displacement')
+    plt.title('Examples for the mean-square displacement')
+    plt.savefig('2.png', dpi=100, bbox_inches='tight')
+    plt.show()
+    
+msd(N=2000)
