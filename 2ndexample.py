10680d80d233e87b1a0ef02dbe769992af3ee11a
Atiyeh996 <atahavorgar@mun.ca>
2021-06-14 15:26:01 +0000

    more matplotlib

--- 2ndexample.py
+++ 2ndexample.py
@@ -8,19 +8,24 @@ import tidynamics
 if len(sys.argv)==2:
     output=sys.argv[1]
 
+plt.style.use('ggplot') 
 
-if __name__ == '__main__':
-
+def corr():
     spacing = np.linspace(-5 * np.pi, 5 * np.pi, num=500)
     s = pd.Series(0.7 * np.random.rand(500) + 0.3 * np.sin(spacing))
     t= pd.Series(0.4 * np.random.rand(500) + 0.7 * np.sin(spacing))
 
 
     acf = tidynamics.correlation(s,t)
-    plt.plot(acf)
+    plt.plot(acf,color="#7b99ff",marker='.' )
+    plt.title("correlation over time")
+    plt.xlabel("correlation")
+    plt.ylabel("time")
+
     plt.savefig(output, dpi=300, bbox_inches='tight')
     plt.show()
 
+    return plt
 
-
+corr()
 
