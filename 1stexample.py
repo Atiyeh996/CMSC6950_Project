b809e873a7737fcdce6c5bf5d905ac0727aa264c
Atiyeh996 <atahavorgar@mun.ca>
2021-06-14 01:48:16 +0000

    change font

--- 1stexample.py
+++ 1stexample.py
@@ -5,11 +5,13 @@ import random
 from numpy import *
 
 
+plt.rcParams['font.family'] = 'sans-serif'
+#plt.rcParams['font.sans-serif'] = ['Tahoma']
 plt.style.use('Solarize_Light2')
-def msd(N):
+if __name__ == "__main__":
+    N=2000
     mean=linspace(0,0,N,dtype=float)
     count=0
-
     dock=range(1,50)
     for i in dock:
         s=[-1,1]
@@ -24,23 +26,20 @@ def msd(N):
 
     l = arange(1,N//2)
 
-    plt.plot(l, mean, label='Random walk (num.)')
+    plt.plot(l, mean,color="#444444",linestyle="--", label='Random walk (num.)')
 
-    plt.plot(l, 2*l, label='Random walk (theo.)')
+    plt.plot(l, 2*l,color="k",linestyle="--", label='Random walk (theo.)')
 
-    plt.plot(l[1:], tidynamics.msd(l)[1:],label='Constant velocity (num.)', ls='--')
-    plt.plot(l[1:], l[1:]**2,label='Constant velocity (theo.)', ls='--')
+    plt.plot(l[1:], tidynamics.msd(l)[1:],color="#bf00ff", marker=',',label='Constant velocity (num.)', ls='--')
+    plt.plot(l[1:], l[1:]**2,color="#eeb704", marker=',' , label='Constant velocity (theo.)', ls='--')
 
-
-    plt.loglog()
     plt.legend()
-    plt.xlabel('time')
-    plt.ylabel('mean square displacement')
-    plt.title('Examples for the mean-square displacement')
-    plt.savefig('2.png', dpi=100, bbox_inches='tight')
+    plt.loglog()
+    plt.plot()
+    plt.xlabel('***time***')
+    plt.ylabel('***mean square displacement***')
+    plt.title('Examples for the mean-square displacement', fontsize=10)
+    plt.savefig('3.png', dpi=100, bbox_inches='tight')
     plt.show()
-    
-msd(N=2000)
 
-if __name__=="__main__":
-    msd(N=2000)
+
