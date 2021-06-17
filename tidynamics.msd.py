59d7b1399d4bbf60d6eefa40d040f4ea61c42139
Atiyeh996 <83696941+Atiyeh996@users.noreply.github.com>
2021-06-17 19:31:02 +0430

    add comments

--- tidynamics.msd.py
+++ tidynamics.msd.py
@@ -13,12 +13,13 @@ plt.rcParams['figure.figsize']=(10,12)
 
 plt.style.use('Solarize_Light2')
 
-
+#generate 0 array with size 2000
 
 try:
     N=2000
     mean=linspace(0,0,N,dtype=float)
     count=0
+   #pick 100 numbers from mean
     dock=range(1,100)
     for i in dock:
         s=[-1,1]
@@ -35,7 +36,7 @@ try:
 
     plt.plot(T, mean,color="#444444",linestyle="--",marker = 'o',ms =5 ,linewidth = '1', label='Random walk (num.)')
 
-    plt.plot(T, 2*T,color="r",linestyle="--",marker = 'o',ms =5,linewidth = '1', label='Random walk (theo.)')
+    plt.plot(T, mean,color="r",linestyle="--",marker = 'o',ms =5,linewidth = '1', label='Random walk (theo.)')
 
 
 
